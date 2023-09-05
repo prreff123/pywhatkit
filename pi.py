e89bfca109f2733adf8be910ca403a42fc21b271
prreff123 <104686576+prreff123@users.noreply.github.com>
2023-09-05 14:37:52 +0530

    pj

---
+++ pi.py
@@ -0,0 +1,10 @@
+import pywhatkit
+pywhatkit.start_server()
+try:
+    pywhatkit.sendwhatmsg("+917389837625", "Helo", 14, 45)
+    print("Sucess")
+except:
+    print("Error Occured!!")    
+# pywhatkit.playonyt("Virat kohli")
+# pywhatkit.sendwhats_image("+918109098647","Pictures/WIN_20210714_13_18_35_Pro.jpg", "Oh! you are?",14,35)
+
